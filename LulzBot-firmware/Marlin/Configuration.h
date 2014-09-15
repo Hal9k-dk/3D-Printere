93e6cce2004ebcfed92a270c0af24d60c1081f94
Mads Chr. Olesen <mads@mchro.dk>
2014-09-15 22:53:32 +0200

    Updated firmware, updated profiles

--- LulzBot-firmware/Marlin/Configuration.h
+++ LulzBot-firmware/Marlin/Configuration.h
@@ -134,7 +134,7 @@
 // 55 is 100k thermistor - ATC Semitec 104GT-2 (Used in ParCan) (1k pullup)
 
 #define TEMP_SENSOR_0 7
-#define TEMP_SENSOR_1 7
+#define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 7
 
